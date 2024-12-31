283feb41087c647164699bc1b49521d93700e2d6
yobibyte <y0b1byte@proton.me>
2024-12-31 13:41:45 +0000

    Implement automatic grammar to ast generation.

--- src/ast.rs
+++ src/ast.rs
@@ -1,33 +1,33 @@
- // defineAst(outputDir, "Expr", Arrays.asList(
- //      "Binary   : Expr left, Token operator, Expr right",
- //      "Grouping : Expr expression",
- //      "Literal  : Object value",
- //      "Unary    : Token operator, Expr right"
- //    ));
- //
- //     private static void defineAst(
-  //     String outputDir, String baseName, List<String> types)
-  //     throws IOException {
-  //   String path = outputDir + "/" + baseName + ".java";
-  //   PrintWriter writer = new PrintWriter(path, "UTF-8");
-  //
-  //   writer.println("package com.craftinginterpreters.lox;");
-  //   writer.println();
-  //   writer.println("import java.util.List;");
-  //   writer.println();
-  //   writer.println("abstract class " + baseName + " {");
-  //
-  //   writer.println("}");
-  //   writer.close();
-  // }
-  //
+use std::fs::File;
+use std::io::{BufWriter, Write};
 
+fn define_ast(output_dir: String, base_name: String, types: Vec<String>) {
+    let path: String = format!("{output_dir}/{base_name}.rs");
+    for el in types {
+        println!("{}", el);
+    }
+    let file = File::create(path).unwrap();
+    let mut writer = BufWriter::new(&file);
+    writeln!(writer, "struct {base_name} {{").unwrap();
+    writeln!(writer, "").unwrap();
+    writeln!(writer, "}}").unwrap();
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     match args.len() {
-        // 0th arg is always the program name.
-        2 => println!("{}", args[1]),
+        2 => {
+            let output_dir = args[1].clone();
+            println!("Output dir: {}", output_dir);
+
+            let grammar: Vec<String> = vec![
+                String::from("Binary   : Expr left, Token operator, Expr right"),
+                String::from("Grouping : Expr expression"),
+                String::from("Literal  : Object value"),
+                String::from("Unary    : Token operator, Expr right"),
+            ];
+            define_ast(output_dir, String::from("Expr"), grammar);
+        }
         _ => println!("Usage: generate_ast [script]"),
     }
 }
